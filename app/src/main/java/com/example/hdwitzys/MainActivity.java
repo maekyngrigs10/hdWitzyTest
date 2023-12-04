87bd90851b0bd11d4384bd6ceb752124f6ccd2bf
Ty Tichenor <tjtichenor14@gmail.com>
2023-12-04 01:24:06 -0600

    orange

--- app/src/main/java/com/example/hdwitzys/MainActivity.java
+++ app/src/main/java/com/example/hdwitzys/MainActivity.java
@@ -1,109 +1,67 @@
 package com.example.hdwitzys;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
-import com.example.hdwitzys.ui.checkout.CheckoutFragment;
-import com.example.hdwitzys.ui.drinks.DrinksFragment;
-import com.example.hdwitzys.ui.entrees.EntreesFragment;
-import com.example.hdwitzys.ui.home.HomeFragment;
-import com.example.hdwitzys.ui.icecream.IceCreamFragment;
-import com.example.hdwitzys.ui.sides.SidesFragment;
-import com.google.android.material.navigation.NavigationView;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.hdwitzys.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-    private Toolbar toolbar;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar);
-        // Initialize your DrawerLayout and NavigationView
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // Set up ActionBarDrawerToggle for handling drawer open/close events
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        // Set up NavigationView item click listener
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                int itemId = item.getItemId();
-
-                if (itemId == R.id.nav_home) {
-                    openHomeFragment();
-                } else if (itemId == R.id.nav_entrees) {
-                    openEntreesFragment();
-                } else if (itemId == R.id.nav_sides) {
-                    openSidesFragment();
-                } else if (itemId == R.id.nav_drinks) {
-                    openDrinksFragment();
-                } else if (itemId == R.id.nav_icecream) {
-                    openIceCreamFragment();
-                } else if (itemId == R.id.nav_checkout) {
-                    openCheckoutFragment();
-                }
-
-                // Close the drawer after handling the click
-                drawerLayout.closeDrawer(GravityCompat.START);
-                return true;
+            public void onClick(View view) {
+                Snackbar.make(view, "fab text fab text", Snackbar.LENGTH_LONG)
+                        .show();
             }
 
+
         });
-    }
-
-    private void openHomeFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame, new HomeFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-    private void openEntreesFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame, new EntreesFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-    private void openSidesFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame, new SidesFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-    private void openDrinksFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame, new DrinksFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-    private void openIceCreamFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame, new IceCreamFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-    private void openCheckoutFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame, new CheckoutFragment());
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_entrees, R.id.nav_sides, R.id.nav_drinks, R.id.nav_icecream, R.id.nav_checkout)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
--- app/src/main/java/com/example/hdwitzys/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hdwitzys/ui/home/HomeFragment.java
@@ -2,36 +2,22 @@ package com.example.hdwitzys.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import androidx.core.view.GravityCompat;
-
 
 import androidx.annotation.NonNull;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.hdwitzys.R;
 import com.example.hdwitzys.databinding.FragmentHomeBinding;
-import com.example.hdwitzys.ui.drinks.DrinksFragment;
-import com.example.hdwitzys.ui.entrees.EntreesFragment;
-import com.example.hdwitzys.ui.icecream.IceCreamFragment;
-import com.example.hdwitzys.ui.checkout.CheckoutFragment;
-import com.example.hdwitzys.ui.sides.SidesFragment;
-import com.google.android.material.navigation.NavigationView;
 
-public class HomeFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener {
+public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
@@ -40,46 +26,7 @@ public class HomeFragment extends Fragment implements NavigationView.OnNavigatio
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
-        // Set up NavigationView listener
-        NavigationView navigationView = root.findViewById(R.id.nav_view); // Use the correct ID here
-        navigationView.setNavigationItemSelectedListener(this);
-
-
         return root;
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            // No need to replace with HomeFragment since you are already in the HomeFragment
-            // Add any specific behavior if needed
-        } else if (id == R.id.nav_entrees) {
-            replaceFragment(new EntreesFragment());
-        } else if (id == R.id.nav_sides) {
-            replaceFragment(new SidesFragment());
-        } else if (id == R.id.nav_drinks) {
-            replaceFragment(new DrinksFragment());
-        } else if (id == R.id.nav_icecream) {
-            replaceFragment(new IceCreamFragment());
-        } else if (id == R.id.nav_checkout) {
-            replaceFragment(new CheckoutFragment());
-        }
-
-        // Close the drawer
-        DrawerLayout drawer = getActivity().findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
     }
 
     @Override
